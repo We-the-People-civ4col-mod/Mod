351ee3653d63ea8552cbc7574e1fded9d4bf630c
Erik Sandbraaten <erik.sandbraaten@gmail.com>
2020-04-03 23:39:20 +0200

    #102 Reset the AI and UI PFs after promoting
    Adapted from:
    https://github.com/karadoc/Civ4-K-Mod/commit/47b75c64afbbe6ad3280ce3a52f6f65b4881cf1e
    https://github.com/karadoc/Civ4-K-Mod/commit/a1b791123da87023768097598a1473abd957d622

--- Project Files/DLLSources/CvUnit.cpp
+++ Project Files/DLLSources/CvUnit.cpp
@@ -7515,11 +7515,14 @@ void CvUnit::promote(PromotionTypes ePromotion, int iLeaderUnitId)
 
 	testPromotionReady();
 
+	CvSelectionGroup::path_finder.Reset(); // K-Mod. (This currently isn't important, because the AI doesn't use promotions mid-turn anyway.)
+
 	if (IsSelected())
 	{
 		gDLL->getInterfaceIFace()->playGeneralSound(GC.getPromotionInfo(ePromotion).getSound());
 
 		gDLL->getInterfaceIFace()->setDirty(UnitInfo_DIRTY_BIT, true);
+		gDLL->getFAStarIFace()->ForceReset(&GC.getInterfacePathFinder()); // K-Mod.
 	}
 	else
 	{
