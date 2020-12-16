944b91c7d32b57dc2e2932697b0f96df5d7c359a
Nightinggale <Nightinggale.coding@gmail.com>
2020-12-16 19:18:15 +0100

    Fixed compilation issues related to not finding perl
    PERL variable can now be set in Makefile.settings to specify the perl binary.
    Compilation will no longer move on to next step if one step fails.

--- Project Files/Compile-Assert.bat
+++ Project Files/Compile-Assert.bat
@@ -3,9 +3,9 @@
 SET PATH=%PATH%;..\..\Compiler\Microsoft Visual C++ Toolkit 2003\bin
 
 set TARGET=Assert
-nmake source_list /NOLOGO
-nmake fastdep /NOLOGO
-nmake precompile  /NOLOGO
+nmake source_list /NOLOGO && ^
+nmake fastdep /NOLOGO && ^
+nmake precompile  /NOLOGO && ^
 bin\jom build
 
 pause
