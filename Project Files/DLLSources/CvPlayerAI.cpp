bb6c413416813e66d5d27aa539379cc015fd1cdb
Erik Sandbraaten <erik.sandbraaten@gmail.com>
2021-05-27 19:09:43 +0200

    Fixed potential OOS issue concerning native bargaining Since the rng draw happens in an async context, it is inappropriate to use getSorenRandNum here
    (cherry picked from commit e4e83e51daa3586ad7582ae7c72fb2168483fb85)

--- Project Files/DLLSources/CvPlayerAI.cpp
+++ Project Files/DLLSources/CvPlayerAI.cpp
@@ -3526,7 +3526,7 @@ bool CvPlayerAI::AI_counterPropose(PlayerTypes ePlayer, const CLinkList<TradeDat
 			if (pNode->m_data.m_eItemType == TRADE_GOLD)
 			{
 				int oldPrice = pNode->m_data.m_iData1;
-				int randomPriceChange = GC.getGameINLINE().getSorenRandNum(priceIncreaseMax, "Natives Price Change Sell");
+				int randomPriceChange = GC.getASyncRand().get(priceIncreaseMax, "Natives Price Change Sell");
 				if (randomPriceChange < priceIncreaseMax / 3)
 				{
 					randomPriceChange = priceIncreaseMax / 3;
@@ -3558,7 +3558,7 @@ bool CvPlayerAI::AI_counterPropose(PlayerTypes ePlayer, const CLinkList<TradeDat
 			if (pNode->m_data.m_eItemType == TRADE_GOLD)
 			{
 				int oldPrice = pNode->m_data.m_iData1;
-				int randomPriceChange = GC.getGameINLINE().getSorenRandNum(priceDecreaseMax, "Natives Price Change Buy");
+				int randomPriceChange = GC.getASyncRand().get(priceDecreaseMax, "Natives Price Change Buy");
 				if (randomPriceChange < priceDecreaseMax / 3)
 				{
 					randomPriceChange = priceDecreaseMax / 3;
