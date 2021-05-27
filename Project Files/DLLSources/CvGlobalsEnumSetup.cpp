We-the-People-civ4col-mod/Mod

commit 08052d73e84a771866c5f0cb52691b5edea6f41b

CivEffects now tells if a player can conquer cities and/or found cities
CivilizationInfo now has a CivCategory tag (european, native, king, barbarian, church)
Each CivCategory is awarded a category specific CivEffect by default.
CivEffects can unlock conquering cities specified by which CivCategory it conquers from.
This puts the controls in one place in xml rather than hardcoded hard to find places in the DLL.
It also makes it easy to prevent a player from ever owning cities.

Fixes#365: crash when barbarian conquers a colony

Project Files/DLLSources/CvGlobalsEnumSetup.cpp
@@ -12,6 +12,8 @@ CivEffectTypes   CIV_EFFECT_DEFAULT_ALL      = NO_CIV_EFFECT;
 CivEffectTypes   CIV_EFFECT_DEFAULT_EUROPEAN = NO_CIV_EFFECT;
 CivEffectTypes   CIV_EFFECT_DEFAULT_NATIVE   = NO_CIV_EFFECT;
 CivEffectTypes   CIV_EFFECT_DEFAULT_KING     = NO_CIV_EFFECT;
+CivEffectTypes   CIV_EFFECT_DEFAULT_BARBARIAN= NO_CIV_EFFECT;
+CivEffectTypes   CIV_EFFECT_DEFAULT_CHURCH   = NO_CIV_EFFECT;
 CivEffectTypes   CIV_EFFECT_DEFAULT_HUMAN    = NO_CIV_EFFECT;
 CivEffectTypes   CIV_EFFECT_DEFAULT_AI       = NO_CIV_EFFECT;
 
@@ -108,6 +110,14 @@ void CvGlobals::postXMLLoad(bool bFirst)
 			{
 				CIV_EFFECT_DEFAULT_KING = eCivEffect;
 			}
+			else if (strcmp(szType, "CIV_EFFECT_DEFAULT_BARBARIAN") == 0)
+			{
+				CIV_EFFECT_DEFAULT_BARBARIAN = eCivEffect;
+			}
+			else if (strcmp(szType, "CIV_EFFECT_DEFAULT_CHURCH") == 0)
+			{
+				CIV_EFFECT_DEFAULT_CHURCH = eCivEffect;
+			}
 			else if (strcmp(szType, "CIV_EFFECT_DEFAULT_HUMAN") == 0)
 			{
 				CIV_EFFECT_DEFAULT_HUMAN = eCivEffect;
@@ -131,6 +141,8 @@ void CvGlobals::postXMLLoad(bool bFirst)
 		DisplayXMLmissingError(CIV_EFFECT_DEFAULT_EUROPEAN   != NO_CIV_EFFECT, "CIV_EFFECT_DEFAULT_EUROPEAN"  );
 		DisplayXMLmissingError(CIV_EFFECT_DEFAULT_NATIVE     != NO_CIV_EFFECT, "CIV_EFFECT_DEFAULT_NATIVE"    );
 		DisplayXMLmissingError(CIV_EFFECT_DEFAULT_KING       != NO_CIV_EFFECT, "CIV_EFFECT_DEFAULT_KING"      );
+		DisplayXMLmissingError(CIV_EFFECT_DEFAULT_BARBARIAN  != NO_CIV_EFFECT, "CIV_EFFECT_DEFAULT_BARBARIAN" );
+		DisplayXMLmissingError(CIV_EFFECT_DEFAULT_CHURCH     != NO_CIV_EFFECT, "CIV_EFFECT_DEFAULT_CHURCH"    );
 		DisplayXMLmissingError(CIV_EFFECT_DEFAULT_HUMAN      != NO_CIV_EFFECT, "CIV_EFFECT_DEFAULT_HUMAN"     );
 		DisplayXMLmissingError(CIV_EFFECT_DEFAULT_AI         != NO_CIV_EFFECT, "CIV_EFFECT_DEFAULT_AI"        );
 

Assets/XML/CivEffects/CIV4CivEffectsInfos.xml
@@ -14,6 +14,20 @@
 	</CivEffectInfo>
 	<CivEffectInfo>
 		<Type>CIV_EFFECT_DEFAULT_EUROPEAN</Type>
+		<TagGroupAllow>
+			<AllowConqueringCities>
+				<AllowConqueringCity>
+					<eCivCategory>CIV_CATEGORY_EUROPEAN</eCivCategory>
+					<iAllow>1</iAllow>
+				</AllowConqueringCity>
+				<AllowConqueringCity>
+					<eCivCategory>CIV_CATEGORY_KING</eCivCategory>
+					<iAllow>1</iAllow>
+				</AllowConqueringCity>
+			</AllowConqueringCities>
+			<iAllowFoundCity>1</iAllowFoundCity>
+		</TagGroupAllow>
+		
 		<TagGroupCity>
 			<iCanUseDomesticMarket>1</iCanUseDomesticMarket>
 		</TagGroupCity>
@@ -23,9 +37,36 @@
 	</CivEffectInfo>
 	<CivEffectInfo>
 		<Type>CIV_EFFECT_DEFAULT_NATIVE</Type>
+		<TagGroupAllow>
+			<AllowConqueringCities>
+				<AllowConqueringCity>
+					<eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
+					<iAllow>1</iAllow>
+				</AllowConqueringCity>
+			</AllowConqueringCities>
+			<iAllowFoundCity>1</iAllowFoundCity>
+		</TagGroupAllow>
 	</CivEffectInfo>
 	<CivEffectInfo>
 		<Type>CIV_EFFECT_DEFAULT_KING</Type>
+		<TagGroupAllow>
+			<AllowConqueringCities>
+				<AllowConqueringCity>
+					<eCivCategory>CIV_CATEGORY_EUROPEAN</eCivCategory>
+					<iAllow>1</iAllow>
+				</AllowConqueringCity>
+				<AllowConqueringCity>
+					<eCivCategory>CIV_CATEGORY_KING</eCivCategory>
+					<iAllow>1</iAllow>
+				</AllowConqueringCity>
+			</AllowConqueringCities>
+		</TagGroupAllow>
+	</CivEffectInfo>
+	<CivEffectInfo>
+		<Type>CIV_EFFECT_DEFAULT_BARBARIAN</Type>
+	</CivEffectInfo>
+	<CivEffectInfo>
+		<Type>CIV_EFFECT_DEFAULT_CHURCH</Type>
 	</CivEffectInfo>
 	<CivEffectInfo>
 		<Type>CIV_EFFECT_DEFAULT_HUMAN</Type>

Assets/XML/CivEffects/CIV4CivEffectsInfosSchema.xml
@@ -7,6 +7,7 @@
 	<ElementType name="eBuild" content="textOnly"/>
 	<ElementType name="eBuildingClass" content="textOnly"/>
 	<ElementType name="eCivic" content="textOnly"/>
+	<ElementType name="eCivCategory" content="textOnly"/>
 	<ElementType name="eImprovement" content="textOnly"/>
 	<ElementType name="eProfession" content="textOnly"/>
 	<ElementType name="ePromotion" content="textOnly"/>
@@ -15,6 +16,7 @@
 	<ElementType name="eYield" content="textOnly"/>
 	
 	<ElementType name="iAllow" content="textOnly" dt:type="int" />
+	<ElementType name="iAllowFoundCity" content="textOnly" dt:type="int" />
 	<ElementType name="iCanUseDomesticMarket" content="textOnly" dt:type="int" />
 	<ElementType name="iNumUnitsOnDockChange" content="textOnly" dt:type="int" />
 	
@@ -33,6 +35,10 @@
 	</ElementType>
 	<ElementType name="AllowCivic" content="eltOnly">
 		<element type="eCivic" />
+		<element type="iAllow" />
+	</ElementType>
+	<ElementType name="AllowConqueringCity" content="eltOnly">
+		<element type="eCivCategory" />
 		<element type="iAllow" />
 	</ElementType>
 	<ElementType name="AllowImmigrant" content="eltOnly">
@@ -76,6 +82,9 @@
 	<ElementType name="AllowCivics" content="eltOnly">
 		<element type="AllowCivic" minOccurs="0" maxOccurs="*" />
 	</ElementType>
+	<ElementType name="AllowConqueringCities" content="eltOnly">
+		<element type="AllowConqueringCity" minOccurs="0" maxOccurs="*" />
+	</ElementType>
 	<ElementType name="AllowImmigrants" content="eltOnly">
 		<element type="AllowImmigrant" minOccurs="0" maxOccurs="*" />
 	</ElementType>
@@ -105,6 +114,7 @@
 		<element type="AllowBuilds" minOccurs="0" />
 		<element type="AllowBuildingClasses" minOccurs="0" />
 		<element type="AllowCivics" minOccurs="0" />
+		<element type="AllowConqueringCities" minOccurs="0" />
 		<element type="AllowImmigrants" minOccurs="0" />
 		<element type="AllowImprovements" minOccurs="0" />
 		<element type="AllowProfessions" minOccurs="0" />
@@ -112,6 +122,7 @@
 		<element type="AllowRoutes" minOccurs="0" />
 		<element type="AllowUnitClasses" minOccurs="0" />
 		<element type="AllowYields" minOccurs="0" />
+		<element type="iAllowFoundCity" minOccurs="0" />
 	</ElementType>
 
 	<ElementType name="TagGroupCity" content="eltOnly">

Assets/XML/Civilizations/CIV4CivilizationInfos.xml
@@ -23,6 +23,7 @@
             <bWaterWorks>1</bWaterWorks>
             <bEurope>0</bEurope>
             <bNative>0</bNative>
+            <eCivCategory>CIV_CATEGORY_EUROPEAN</eCivCategory>
             <iAdvancedStartPoints>0</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -490,6 +491,7 @@
             <bWaterWorks>1</bWaterWorks>
             <bEurope>0</bEurope>
             <bNative>0</bNative>
+            <eCivCategory>CIV_CATEGORY_EUROPEAN</eCivCategory>
             <iAdvancedStartPoints>0</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -963,6 +965,7 @@
             <bWaterWorks>1</bWaterWorks>
             <bEurope>0</bEurope>
             <bNative>0</bNative>
+            <eCivCategory>CIV_CATEGORY_EUROPEAN</eCivCategory>
             <iAdvancedStartPoints>0</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -1408,6 +1411,7 @@
             <bWaterWorks>1</bWaterWorks>
             <bEurope>0</bEurope>
             <bNative>0</bNative>
+            <eCivCategory>CIV_CATEGORY_EUROPEAN</eCivCategory>
             <iAdvancedStartPoints>0</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -1798,6 +1802,7 @@
             <bWaterWorks>1</bWaterWorks>
             <bEurope>0</bEurope>
             <bNative>0</bNative>
+            <eCivCategory>CIV_CATEGORY_EUROPEAN</eCivCategory>
             <iAdvancedStartPoints>0</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -2166,6 +2171,7 @@
             <bWaterWorks>1</bWaterWorks>
             <bEurope>0</bEurope>
             <bNative>0</bNative>
+            <eCivCategory>CIV_CATEGORY_EUROPEAN</eCivCategory>
             <iAdvancedStartPoints>0</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -2487,6 +2493,7 @@
             <bWaterWorks>1</bWaterWorks>
             <bEurope>0</bEurope>
             <bNative>0</bNative>
+            <eCivCategory>CIV_CATEGORY_EUROPEAN</eCivCategory>
             <iAdvancedStartPoints>0</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -2842,6 +2849,7 @@
             <bWaterWorks>1</bWaterWorks>
             <bEurope>0</bEurope>
             <bNative>0</bNative>
+            <eCivCategory>CIV_CATEGORY_EUROPEAN</eCivCategory>
             <iAdvancedStartPoints>0</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -3364,6 +3372,7 @@
             <bWaterWorks>1</bWaterWorks>
             <bEurope>1</bEurope>
             <bNative>0</bNative>
+            <eCivCategory>CIV_CATEGORY_KING</eCivCategory>
             <iAdvancedStartPoints>0</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -3642,6 +3651,7 @@
             <bWaterWorks>1</bWaterWorks>
             <bEurope>1</bEurope>
             <bNative>0</bNative>
+            <eCivCategory>CIV_CATEGORY_KING</eCivCategory>
             <iAdvancedStartPoints>0</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -3907,6 +3917,7 @@
             <bWaterWorks>1</bWaterWorks>
             <bEurope>1</bEurope>
             <bNative>0</bNative>
+            <eCivCategory>CIV_CATEGORY_KING</eCivCategory>
             <iAdvancedStartPoints>0</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -4169,6 +4180,7 @@
             <bWaterWorks>1</bWaterWorks>
             <bEurope>1</bEurope>
             <bNative>0</bNative>
+            <eCivCategory>CIV_CATEGORY_KING</eCivCategory>
             <iAdvancedStartPoints>0</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -4434,6 +4446,7 @@
             <bWaterWorks>1</bWaterWorks>
             <bEurope>1</bEurope>
             <bNative>0</bNative>
+            <eCivCategory>CIV_CATEGORY_KING</eCivCategory>
             <iAdvancedStartPoints>0</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -4683,6 +4696,7 @@
             <bWaterWorks>1</bWaterWorks>
             <bEurope>1</bEurope>
             <bNative>0</bNative>
+            <eCivCategory>CIV_CATEGORY_KING</eCivCategory>
             <iAdvancedStartPoints>0</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -4933,6 +4947,7 @@
             <bWaterWorks>1</bWaterWorks>
             <bEurope>1</bEurope>
             <bNative>0</bNative>
+            <eCivCategory>CIV_CATEGORY_KING</eCivCategory>
             <iAdvancedStartPoints>0</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -5184,6 +5199,7 @@
             <bWaterWorks>1</bWaterWorks>
             <bEurope>1</bEurope>
             <bNative>0</bNative>
+            <eCivCategory>CIV_CATEGORY_KING</eCivCategory>
             <iAdvancedStartPoints>0</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -5439,6 +5455,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bSouthAmericanNative>1</bSouthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>7500</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -6386,6 +6403,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bCentralAmericanNative>1</bCentralAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -7339,6 +7357,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bCentralAmericanNative>1</bCentralAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -8309,6 +8328,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bSouthAmericanNative>1</bSouthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -9272,6 +9292,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bNorthAmericanNative>1</bNorthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>7500</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -10239,6 +10260,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bNorthAmericanNative>1</bNorthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -11197,6 +11219,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bNorthAmericanNative>1</bNorthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -12152,6 +12175,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bNorthAmericanNative>1</bNorthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -13107,6 +13131,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bCentralAmericanNative>1</bCentralAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>7500</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -14083,6 +14108,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bSouthAmericanNative>1</bSouthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -15051,6 +15077,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bSouthAmericanNative>1</bSouthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -16008,6 +16035,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bNorthAmericanNative>1</bNorthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -16986,6 +17014,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bNorthAmericanNative>1</bNorthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -17945,6 +17974,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bCentralAmericanNative>1</bCentralAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -18911,6 +18941,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bSouthAmericanNative>1</bSouthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -19873,6 +19904,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bCentralAmericanNative>1</bCentralAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -20827,6 +20859,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bNorthAmericanNative>1</bNorthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -21786,6 +21819,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bNorthAmericanNative>1</bNorthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>5000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -22754,6 +22788,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bSouthAmericanNative>1</bSouthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -23698,6 +23733,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bCentralAmericanNative>1</bCentralAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -24666,6 +24702,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bSouthAmericanNative>1</bSouthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -25615,6 +25652,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bNorthAmericanNative>1</bNorthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>4000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -26565,6 +26603,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bNorthAmericanNative>1</bNorthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>4000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -27518,6 +27557,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bSouthAmericanNative>1</bSouthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -28462,6 +28502,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bNorthAmericanNative>1</bNorthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -29420,6 +29461,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bNorthAmericanNative>1</bNorthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>4000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -30374,6 +30416,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bCentralAmericanNative>1</bCentralAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -31325,6 +31368,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bCentralAmericanNative>1</bCentralAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -32280,6 +32324,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bSouthAmericanNative>1</bSouthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -33225,6 +33270,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bSouthAmericanNative>1</bSouthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>
@@ -34167,6 +34213,7 @@
             <bWaterWorks>1</bWaterWorks>
             <bEurope>0</bEurope>
             <bNative>0</bNative>
+            <eCivCategory>CIV_CATEGORY_BARBARIAN</eCivCategory>
             <iAdvancedStartPoints>0</iAdvancedStartPoints>
             <iAreaMultiplier>0</iAreaMultiplier>
             <iDensityMultiplier>0</iDensityMultiplier>
@@ -34218,6 +34265,7 @@
             <bWaterWorks>1</bWaterWorks>
             <bEurope>0</bEurope>
             <bNative>0</bNative>
+            <eCivCategory>CIV_CATEGORY_CHURCH</eCivCategory>
             <iAdvancedStartPoints>0</iAdvancedStartPoints>
             <iAreaMultiplier>0</iAreaMultiplier>
             <iDensityMultiplier>0</iDensityMultiplier>
@@ -34266,6 +34314,7 @@
             <bEurope>0</bEurope>
             <bNative>1</bNative>
             <bNorthAmericanNative>1</bNorthAmericanNative>
+            <eCivCategory>CIV_CATEGORY_NATIVE</eCivCategory>
             <iAdvancedStartPoints>3000</iAdvancedStartPoints>
             <iAreaMultiplier>100</iAreaMultiplier>
             <iDensityMultiplier>100</iDensityMultiplier>

Assets/XML/Civilizations/CIV4CivilizationsSchema.xml
@@ -9,6 +9,7 @@
 	<ElementType name="Strategy" content="textOnly"/>
 	<ElementType name="ArtDefineTag" content="textOnly"/>
 	<ElementType name="AlarmType" content="textOnly"/>
+	<ElementType name="eCivCategory" content="textOnly"/>
 	<ElementType name="eCivEffect" content="textOnly"/>
 	<ElementType name="iBaseAttitude" content="textOnly" dt:type="int"/>
 	<ElementType name="iNativeAttitude" content="textOnly" dt:type="int"/>
@@ -547,6 +548,7 @@
 		<element type="bNorthAmericanNative" minOccurs="0"/>
 		<element type="bSouthAmericanNative" minOccurs="0"/>
 		<element type="bCentralAmericanNative" minOccurs="0"/>
+		<element type="eCivCategory" minOccurs="0"/>
 		<element type="eCivEffect" minOccurs="0"/>
 		<element type="iAdvancedStartPoints"/>
 		<element type="iAreaMultiplier"/>

Project Files/DLLSources/CvCity.cpp
@@ -8956,7 +8956,11 @@ bool CvCity::isAutoRaze() const
 
 	if (getPreviousOwner() != NO_PLAYER)
 	{
-		if (GET_PLAYER(getPreviousOwner()).isNative() != isNative())
+		// set allow conquer to CivEffect data rather than hardcoding - Nightinggale
+		CvPlayerAI& kPrevOwner = GET_PLAYER(getPreviousOwner());
+		CvPlayerAI& kOwner = GET_PLAYER(getOwnerINLINE());
+
+		if (!kOwner.canConquerCity(kPrevOwner.getCivCategoryTypes()))
 		{
 			return true;
 		}

Project Files/DLLSources/CvEnums.h
@@ -661,6 +661,20 @@ enum PlotTypes
 #endif
 };
 
+enum CivCategoryTypes
+{
+	NO_CIV_CATEGORY = -1,
+
+	CIV_CATEGORY_EUROPEAN,
+	CIV_CATEGORY_NATIVE,
+	CIV_CATEGORY_KING,
+	CIV_CATEGORY_BARBARIAN,
+	CIV_CATEGORY_CHURCH,
+	CIV_CATEGORY_NOT_SET,
+
+	NUM_CIV_CATEGORY_TYPES,
+};
+
 /*
 enum GameOptionTypes
 {
@@ -764,6 +778,7 @@ enum TileArtTypes
 	NUM_TILE_ART_TYPES
 };
 
+// exclusively used by the exe related to terrain art
 enum TerrainGroupTypes
 {
 	TERRAIN_GROUP_NONE = -1,
@@ -2628,6 +2643,8 @@ extern CivEffectTypes CIV_EFFECT_DEFAULT_ALL;
 extern CivEffectTypes CIV_EFFECT_DEFAULT_EUROPEAN;
 extern CivEffectTypes CIV_EFFECT_DEFAULT_NATIVE;
 extern CivEffectTypes CIV_EFFECT_DEFAULT_KING;
+extern CivEffectTypes CIV_EFFECT_DEFAULT_BARBARIAN;
+extern CivEffectTypes CIV_EFFECT_DEFAULT_CHURCH;
 extern CivEffectTypes CIV_EFFECT_DEFAULT_HUMAN;
 extern CivEffectTypes CIV_EFFECT_DEFAULT_AI;
 #endif
@@ -2710,6 +2727,7 @@ enum JITarrayTypes
 	JIT_ARRAY_MISSION_AI,
 	JIT_ARRAY_STRATEGY,
 	JIT_ARRAY_PLOT_TYPE,
+	JIT_ARRAY_CIV_CATEGORY,
 	
 	// types used by InfoArray
 	JIT_ARRAY_NO_TYPE,

commit c3ffaecb88a6878a348635ff55960a1ec06b7f8a

Fixed error message at startup about missing CIV_EFFECT_DEFAULT_HUMAN
Introduced in 08052d73e84a771866c5f0cb52691b5edea6f41b

Project Files/DLLSources/CvGlobalsEnumSetup.cpp
@@ -131,7 +131,7 @@ void CvGlobals::postXMLLoad(bool bFirst)
 				continue;
 			}
 			++iCounter;
-			if (iCounter == 6)
+			if (iCounter == 8)
 			{
 				// all found. No need to loop the rest
 				break;