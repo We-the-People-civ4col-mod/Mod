bf7ef132a8bb28a8164cd4daa1f38324ae2ed2f2
Erik Sandbraaten <erik.sandbraaten@gmail.com>
2021-06-04 09:08:35 +0200

    WIP more logging

--- Project Files/DLLSources/CvPlayerAI.cpp
+++ Project Files/DLLSources/CvPlayerAI.cpp
@@ -27,6 +27,8 @@
 #include "CvTradeRoute.h"
 
 #include "CvSavegame.h"
+#include "BetterBTSAI.h"
+
 #pragma push_macro("free")  
 #pragma push_macro("new")  
 #undef free
@@ -8076,6 +8078,9 @@ void CvPlayerAI::AI_doProfessions()
 						{
 							pLoopCity->removePopulationUnit(pBestUnit, false, eProfession);
 							pBestUnit->AI_setUnitAIType(eUnitAI);
+							if (gPlayerLogLevel >= 1) logBBAI("CvPlayerAI::AI_doProfessions Player (%S)'s City (%S) emits civilian Unit (%S)",
+								getCivilizationDescription(), pLoopCity->getNameKey(), pBestUnit->getNameOrProfessionKey());
+
 						}
 					}
 				}
@@ -8142,6 +8147,8 @@ void CvPlayerAI::AI_doProfessions()
 						{
 							pLoopCity->removePopulationUnit(pBestUnit, false, eProfession);
 							pBestUnit->AI_setUnitAIType(UNITAI_DEFENSIVE);
+							if (gPlayerLogLevel >= 1) logBBAI("CvPlayerAI::AI_doProfessions Player (%S)'s City (%S) emits military Unit (%S)",
+								getCivilizationDescription(), pLoopCity->getNameKey(), pBestUnit->getNameOrProfessionKey());
 						}
 						else
 						{
