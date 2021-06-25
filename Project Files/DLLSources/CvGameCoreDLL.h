00f0cdd2d75548c3a26ba09f077ac1779a1888ca
Erik Sandbraaten <erik.sandbraaten@gmail.com>
2021-06-25 08:32:41 +0200

    The debug heap is now only used for debug builds (This was an unintended change that has now been reverted)

--- Project Files/DLLSources/CvGameCoreDLL.h
+++ Project Files/DLLSources/CvGameCoreDLL.h
@@ -19,7 +19,6 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <MMSystem.h>
-#include <crtdbg.h>
 #if defined _DEBUG && !defined USE_MEMMANAGER
 //#define USE_MEMMANAGER
 //#include <crtdbg.h>
