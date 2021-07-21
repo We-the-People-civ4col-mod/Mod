508b44d2345040631cba71681191a545352bc80a
Erik Sandbraaten <erik.sandbraaten@gmail.com>
2021-07-21 09:19:36 +0200

    Fix missing initialization of m_bHasHurried m_bHasHurried is now reset by by CvCity::popOrder

--- Project Files/DLLSources/CvCityAI.cpp
+++ Project Files/DLLSources/CvCityAI.cpp
@@ -2267,6 +2267,9 @@ void CvCityAI::AI_doHurry(bool bForce)
 	PROFILE_FUNC();
 	FAssert(!isHuman() || isProductionAutomated());
 	
+	// Previously hurried builds must have been processed before considering another hurry
+	FAssert(!m_bHasHurried);
+
 	// TODO: Cache this
 	HurryTypes eGoldHurry = NO_HURRY;
 	for (int i = FIRST_HURRY; i < GC.getNumHurryInfos(); ++i)
