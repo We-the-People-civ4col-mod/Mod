213d8298f30616b54a38357f2a8f667e3bf548aa
raystuttgart <reinhold.volk@gmx.de>
2023-08-19 22:36:56 +0200

    Also fixed the Citizen Automation Buttons

--- Assets/Python/Screens/CvMainInterface.py
+++ Assets/Python/Screens/CvMainInterface.py
@@ -866,7 +866,7 @@ class CvMainInterface:
 		self.appendtoHideState(screen, "LockCitizens", HIDE_TYPE_CITY, HIDE_LEVEL_HIDE)
 
 	# RESOURCE TABLE
-		screen.setImageButton("AutomateCitizens", ArtFileMgr.getInterfaceArtInfo("INTERFACE_CITY_AUTOMATE_CITIZENS").getPath(), yResolution - BOTTOM_CENTER_HUD_HEIGHT, xResolution - SMALL_BUTTON_SIZE, SMALL_BUTTON_SIZE * 2, SMALL_BUTTON_SIZE * 2, WidgetTypes.WIDGET_AUTOMATE_CITIZENS, 1, -1)
+		#screen.setImageButton("AutomateCitizens", ArtFileMgr.getInterfaceArtInfo("INTERFACE_CITY_AUTOMATE_CITIZENS").getPath(), yResolution - BOTTOM_CENTER_HUD_HEIGHT, xResolution - SMALL_BUTTON_SIZE, SMALL_BUTTON_SIZE * 2, SMALL_BUTTON_SIZE * 2, WidgetTypes.WIDGET_AUTOMATE_CITIZENS, 1, -1)
 		#self.appendtoHideState(screen, "AutomateCitizens", HIDE_TYPE_CITY, HIDE_LEVEL_HIDE)
 		screen.addScrollPanel("ResourceTable", u"", 0, yResolution - BOTTOM_CENTER_HUD_HEIGHT, xResolution, BOTTOM_CENTER_HUD_HEIGHT, PanelStyles.PANEL_STYLE_STANDARD, false, WidgetTypes.WIDGET_GENERAL, -1, -1 )
 		screen.hide("ResourceTable")
@@ -2950,6 +2950,9 @@ class CvMainInterface:
 			screen.show("CityGarrisonPanel")
 			screen.show("CityTransportPanel")
 			screen.moveToFront("ShowOrHideYields")
+			screen.moveToFront("AutomateProduction")
+			screen.moveToFront("AutomateCitizens")
+			screen.moveToFront("LockCitizens")
 			for iYield in self.TableYields:
 				screen.moveToFront("YieldIcon" + str(iYield))
 		else:
