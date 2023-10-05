0453442ff7e03b56e84f3b43f5c505130f551883
Johan Buret <johan.buret@free.fr>
2023-10-05 23:15:32 +0200

    CvPromotionInfo now owns if a promotion should be presented to a defensive unit

--- Project Files/DLLSources/CvInfos.h
+++ Project Files/DLLSources/CvInfos.h
@@ -267,6 +267,7 @@ public:
 	bool isEnemyRoute() const;
 	bool isAlwaysHeal() const;
 	bool isHillsDoubleMove() const;
+	bool isAvailableForDefensiveUnit(); //WTP, jbu - recalculates once per load;
 
 	const char* getSound() const;
 	void setSound(const char* szVal);
@@ -330,7 +331,9 @@ protected:
 	bool m_bEnemyRoute;
 	bool m_bAlwaysHeal;
 	bool m_bHillsDoubleMove;
-
+	bool m_bAvailableForDefensiveUnit;
+	bool m_bAvailableForDefensiveUnitCached;
+	void calculateAvailableForDefensiveUnit();
 	CvString m_szSound;
 	// Arrays
 	int* m_aiTerrainAttackPercent;
