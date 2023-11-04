22a8fde01d15f9465265707b285e0bdc65415796
Johan Buret <johan.buret@free.fr>
2023-11-04 15:11:02 +0100

    Working copying script

--- Project Files/XMLTextTagCompare.py
+++ Project Files/XMLTextTagCompare.py
@@ -4,6 +4,7 @@ import xml.sax.handler
 import xml.sax.xmlreader
 from pprint import  pprint
 import os
+import os.path
 import sys
 
 class Civ4TextXMLHandler(xml.sax.ContentHandler):
@@ -12,16 +13,22 @@ class Civ4TextXMLHandler(xml.sax.ContentHandler):
         self.within_tag = None
         self.dict_to_fill = dict_to_fill
         self.filename = filename
+        self.locator = None
+        self.start_end = None
+        self.current_Tag_content = None
         #super(type(Civ4TextXMLHandler)).__init__(type(Civ4TextXMLHandler))
 
     def setDocumentLocator(self, locator):
-        pass
+        self.locator = locator
 
     def startDocument(self):
-        #print "Starting parsing %s"%(self.filename)
+     #   print "Starting parsing %s"%(self.filename)
         pass
 
     def startElement(self, name, attrs):
+        if name == "TEXT":
+            self.start_end = self.locator.getLineNumber()
+            self.current_Tag_content = ""
         if name in ["Tag","English","French","Russian"]:
             self.within_tag = name
 
@@ -31,18 +38,99 @@ class Civ4TextXMLHandler(xml.sax.ContentHandler):
             pass
 
         if  self.within_tag == "Tag":
-            self.dict_to_fill[content] = self.filename
+            self.current_Tag_content += content
+
+            self.last_tag = content
 
 
     def endElement(self, name):
-            self.within_tag = None
-
+        self.within_tag = None
+        if name == "TEXT":
+            self.current_tag["line_end"] = self.locator.getLineNumber()
+        if name == "Tag":
+            self.current_tag = {"FILE": self.filename, "line_start": self.start_end, "line_end": None}
+            self.dict_to_fill[ self.current_Tag_content] = self.current_tag
+            self.last_tag = self.current_Tag_content
     def endDocument(self):
         pass
 
+class LineCopier:
+    """This class  msut only output UTF-8 files.It is not XML aware"""
+    destination_encoding = "utf8"
 
-tags_from_mod = dict()
-tags_from_vanilla = dict()
+    opening_text_tag = "<Civ4GameText xmlns=\"http://www.firaxis.com\">"
+    closing_text_tag = "</Civ4GameText>"
+
+    def __init__(self,dest_file_name, vanilla_folder):
+
+        self.dest_file_name = dest_file_name
+        self.dest_file = None
+
+        self.vanilla_folder = vanilla_folder
+        self.current_vanilla_file_name = None
+        self.current_vanilla_file = None
+        self.current_vanilla_line = 0
+
+    def init_dest_file(self):
+        self.dest_file = codecs.open(self.dest_file_name, mode="wb", encoding=self.destination_encoding)
+        self.dest_file.write(self.opening_text_tag+"\r\n")
+
+    def open_new_file(self, new_vanilla_file_name):
+        assert new_vanilla_file_name != self.current_vanilla_file_name,"We try to reopen a file we're already on %s"%new_vanilla_file_name
+
+        if self.current_vanilla_file != None:
+            self.current_vanilla_file.close()
+
+        path_to_open = os.path.join(self.vanilla_folder, new_vanilla_file_name)
+
+        self.current_vanilla_file_name = new_vanilla_file_name
+        self.current_vanilla_file = codecs.open(path_to_open,"r",encoding="cp1252")
+        self.current_vanilla_line = 0
+
+    def process_extract_list_entry(self,entry):
+        file_name, start, end, tag = entry
+
+        if file_name != self.current_vanilla_file_name:
+            self.open_new_file(file_name)
+
+        content = ""
+        while self.current_vanilla_line < start:
+            content = self.current_vanilla_file.readline()
+            self.current_vanilla_line += 1
+
+        #print content
+        #if u"<TEXT>" in content:
+        #    print "yes!"
+
+        #assert u"<TEXT>" in content, "we should get a <TEXT> start" #rthis one fails and yet the condition abive doesn't
+        while self.current_vanilla_line < end :
+            self.dest_file.write(content)
+            content = self.current_vanilla_file.readline()
+            self.current_vanilla_line += 1
+
+        # assert u"</TEXT>" in content, "we should get a </TEXT> end" #this one fails too. WHY?
+        self.dest_file.write(content)
+        self.dest_file.flush()
+
+    def process_extract_list(self,entries):
+        self.init_dest_file()
+
+        for entry in entries:
+            self.process_extract_list_entry(entry)
+
+        self.close_dest_file()
+
+    def close_dest_file(self):
+        "places the right closing tags and close the files"
+
+        self.dest_file.write(self.closing_text_tag + "\r\n")
+        self.dest_file.close()
+
+        if not self.current_vanilla_file.closed:
+            self.current_vanilla_file.close()
+
+
+
 
 
 
@@ -57,34 +145,53 @@ def get_tags_from_text(filename,tags):
     xml.sax.parse(input_source, handler)
 
 
+def used_in_mod(key):
+    if "SAN_MARTIN" in key: ## we don't have San Martin as a leader
+        return False
+    return True
 
-start =  os.getcwd() ##assumed to be "Project Files"
 
-mod_folder = r"..\Assets\XML\Text"
-os.chdir(mod_folder)
+def make_tags_list(text_folder, tags_to_fill):
 
-for root, dir, files  in os.walk("."):
-    for file in files:
-        #print "MOD",file
-        if file[-4:] != ".xml":
-            continue
-        get_tags_from_text(file, tags_from_mod)
+    os.chdir(text_folder)
+    for root, dir, files in os.walk("."):
+        for file in files:
+            # print "MOD",file
+            if file[-4:] != ".xml":
+                continue
+            get_tags_from_text(file, tags_to_fill)
+    os.chdir(start)
 
-os.chdir(start)
-print start
 
-vanilla_folder = "..\..\..\Assets\XML\Text"
 
-os.chdir(vanilla_folder)
-for root, dir, files  in os.walk("."):
-    for file in files:
-        #print "VANILLA", file
-        if file[-4:] != ".xml":
-            continue
-        get_tags_from_text(file, tags_from_vanilla)
+if __name__ == "__main__":
+    tags_from_mod = dict()
+    tags_from_vanilla = dict()
 
-print "##############"
-for key in tags_from_vanilla:
-    if not key in tags_from_mod:
-        print key+"\t"+tags_from_vanilla[key]
+    mod_text_folder = r"..\Assets\XML\Text"
+    vanilla_text_folder = "..\..\..\Assets\XML\Text"
+
+    start =  os.getcwd() ##assumed to be "Project Files"
+
+    make_tags_list(mod_text_folder, tags_from_mod)
+    make_tags_list(vanilla_text_folder, tags_from_vanilla)
+
+    print "##############"
+    os.chdir(start)
+
+    extracts = list()
+    print "TXT_KEY\tFILE\tLINE_START\tLINE_END"
+    for key in tags_from_vanilla:
+        if not key in tags_from_mod and used_in_mod(key):
+            value = tags_from_vanilla[key]
+            print key+"\t"+value["FILE"]+"\t"+value["line_start"].__str__()+"\t"+value["line_end"].__str__()
+            extracts.append((value["FILE"],value["line_start"],value["line_end"],key))
+
+    extracts.sort()
+    print len(extracts)
+
+    dest_file_name = "Vanilla_TEXT_XML_key.xml"
+    line_copier = LineCopier(dest_file_name,vanilla_text_folder)
+    line_copier.process_extract_list(extracts)
+
 
