4f8453ef19ae2f4587d5b7e765cc5de60eb0a01e
Nightinggale <Nightinggale.coding@gmail.com>
2023-11-12 21:32:02 +0100

    Typo fix in Port Royal screen (introduced today)

--- Assets/Python/Screens/CvPortRoyalScreen.py
+++ Assets/Python/Screens/CvPortRoyalScreen.py
@@ -578,7 +578,7 @@ class CvPortRoyalScreen:
 				iPrice *= self.playerEurope.getYieldPortRoyalBuyPrice(iYield)
 				szBuffer = u"%d%c" % (player.getTradeMessageAmount(i), gc.getYieldInfo(iYield).getChar())
 				szAction = localText.getText("TXT_KEY_EU_TRADE_LOG_SOLD", ())
-				if GLobalDefines.PORT_ROYAL_PORT_TAX > 0:
+				if GlobalDefines.PORT_ROYAL_PORT_TAX > 0:
 					szTax = str(iPrice * GLobalDefines.PORT_ROYAL_PORT_TAX / 100)
 			elif player.getTradeMessageType(i) == TradeMessageTypes.TRADE_MESSAGE_EUROPE_YIELD_BOUGHT:
 				iPrice *= self.playerEurope.getYieldPortRoyalSellPrice(iYield)
