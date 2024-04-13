1b4d2b387a987e636ec80782ab9ebdc391d513f9
Schmiddie <cola-rum-schmidt@web.de>
2024-04-13 19:37:08 +0200

    One new native flavor event.

--- Assets/Python/EntryPoints/CvRandomEventInterface.py
+++ Assets/Python/EntryPoints/CvRandomEventInterface.py
@@ -3061,16 +3061,85 @@ def isNativeVillageAndHumanTrade(argsList):
 		return False
 	return True
 
-def canTriggerNativeMilitaryDragoon(argsList):
+####### Stirred up natives events ########
+
+def canTriggerStirredUpNatives(argsList):
 	kTriggeredData = argsList[0]
 	player = gc.getPlayer(kTriggeredData.ePlayer)
-	iAchieve = gc.getInfoTypeForString("ACHIEVE_FOUR_COLONIES")
+	iAchieve = gc.getInfoTypeForString("ACHIEVE_COLONIAL_CAVALRY")
 	#CyInterface().addImmediateMessage("iAchieve "+str(iAchieve), "")
 	if player.isAchieveGained(iAchieve):
 		return True
 	return False
 
 
+def canTriggerStirredUpNativesHorses1(argsList):
+	kTriggeredData = argsList[0]
+	player = gc.getPlayer(kTriggeredData.ePlayer)
+	if not player.isPlayable():
+		return False
+	city = player.getCity(kTriggeredData.iCityId)
+	unit = player.getUnit(kTriggeredData.iUnitId)
+	if city.getX() == unit.getX() and city.getY() == unit.getY():
+		return True
+	return False
+
+def canTriggerStirredUpNativesHorses(argsList):
+	kTriggeredData = argsList[0]
+	player = gc.getPlayer(kTriggeredData.ePlayer)
+	if not player.isPlayable():
+		return False
+	city = player.getCity(kTriggeredData.iCityId)
+	player2 = gc.getPlayer(kTriggeredData.eOtherPlayer)
+	if player.isNone() or player2.isNone() :
+		return False
+	if city.isNone():
+		return False
+	# Read Parameter 1 from the first event and check if enough yield is stored in city
+	eEvent1 = gc.getInfoTypeForString("EVENT_STIRRED_UP_NATIVES_HORSES_1")
+	event1 = gc.getEventInfo(eEvent1)
+	iYield = gc.getInfoTypeForString("YIELD_HORSES")
+	quantity = event1.getGenericParameter(1)
+	Speed = gc.getGameSpeedInfo(CyGame().getGameSpeedType())
+	quantity = quantity * Speed.getStoragePercent()/100
+	if city.getYieldStored(iYield) < -quantity :
+		return False
+	return True
+
+def applyStirredUpNativesHorses(argsList):
+	eEvent = argsList[0]
+	event = gc.getEventInfo(eEvent)
+	kTriggeredData = argsList[1]
+	player = gc.getPlayer(kTriggeredData.ePlayer)
+	city = player.getCity(kTriggeredData.iCityId)
+	player2 = gc.getPlayer(kTriggeredData.eOtherPlayer)
+	nativecity = player2.getCity(kTriggeredData.iOtherPlayerCityId)
+	iYield = gc.getInfoTypeForString("YIELD_HORSES")
+	quantity = event.getGenericParameter(1)
+	Speed = gc.getGameSpeedInfo(CyGame().getGameSpeedType())
+	quantity = quantity * Speed.getStoragePercent()/100
+	if city.getYieldStored(iYield) < -quantity:
+		return
+	city.changeYieldStored(iYield, quantity)
+	nativecity.changeYieldStored(iYield, -quantity)
+
+def getHelpStirredUpNativesHorses(argsList):
+	eEvent = argsList[0]
+	event = gc.getEventInfo(eEvent)
+	kTriggeredData = argsList[1]
+	player = gc.getPlayer(kTriggeredData.ePlayer)
+	city = player.getCity(kTriggeredData.iCityId)
+	player2 = gc.getPlayer(kTriggeredData.eOtherPlayer)
+	nativecity = player2.getCity(kTriggeredData.iOtherPlayerCityId)
+	iYield = gc.getInfoTypeForString("YIELD_HORSES")
+	quantity = event.getGenericParameter(1)
+	Speed = gc.getGameSpeedInfo(CyGame().getGameSpeedType())
+	quantity = quantity * Speed.getStoragePercent()/100
+	szHelp = ""
+	if event.getGenericParameter(1) <> 0 :
+		szHelp = localText.getText("TXT_KEY_EVENT_YIELD_LOOSE", (quantity,  gc.getYieldInfo(iYield).getChar(), city.getNameKey()))
+		szHelp += "\n" + localText.getText("TXT_KEY_EVENT_YIELD_GAIN", (-quantity,  gc.getYieldInfo(iYield).getChar(), nativecity.getNameKey()))
+	return szHelp
 
 ######## Initial Native Trade Event ###########
 
