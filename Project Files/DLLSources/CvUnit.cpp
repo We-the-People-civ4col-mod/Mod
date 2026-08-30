77749e0d882fdf3c3592b9b18ba2c32996676c5e
Schmiddie <cola-rum-schmidt@web.de>
2026-08-30 14:01:04 +0200

    Fix treasure gifts not affecting diplomacy
    Treasure gifts were previously valued based on the unit's static yield costs, ignoring the actual treasure value stored in the unit. As a result, gifting treasures had little or no positive diplomatic effect.
    
    Treasure gifts now use 50% of their actual stored value for the peacetime grant value. Regular unit gifts remain unchanged.

--- Project Files/DLLSources/CvUnit.cpp
+++ Project Files/DLLSources/CvUnit.cpp
@@ -4727,11 +4727,22 @@ void CvUnit::gift(bool bTestTransport)
 	pGiftUnit->convert(this, true);
 
 	int iUnitValue = 0;
-	for (YieldTypes eYield = FIRST_YIELD; eYield < NUM_YIELD_TYPES; ++eYield)
+
+	if (pGiftUnit->getUnitInfo().isTreasure())
 	{
-		iUnitValue += pGiftUnit->getUnitInfo().getYieldCost(eYield);
+		iUnitValue = pGiftUnit->getYieldStored() / 2;
 	}
-	GET_PLAYER(pGiftUnit->getOwnerINLINE()).AI_changePeacetimeGrantValue(eOwner, iUnitValue / 5);
+	else
+	{
+		for (YieldTypes eYield = FIRST_YIELD; eYield < NUM_YIELD_TYPES; ++eYield)
+		{
+			iUnitValue += pGiftUnit->getUnitInfo().getYieldCost(eYield);
+		}
+
+		iUnitValue /= 5;
+	}
+
+	GET_PLAYER(pGiftUnit->getOwnerINLINE()).AI_changePeacetimeGrantValue(eOwner, iUnitValue);
 
 	szBuffer = gDLL->getText("TXT_KEY_MISC_GIFTED_UNIT_TO_YOU", GET_PLAYER(eOwner).getNameKey(), pGiftUnit->getNameKey());
 	gDLL->UI().addPlayerMessage(pGiftUnit->getOwnerINLINE(), false, GC.getEVENT_MESSAGE_TIME(), szBuffer, "AS2D_UNITGIFTED", MESSAGE_TYPE_INFO, pGiftUnit->getButton(), COLOR_WHITE, pGiftUnit->getX_INLINE(), pGiftUnit->getY_INLINE(), true, true);
